eaaaba5befbb778523475b3ef0edb2463a3b8f20
equinox-1092 <sdiaz1234di@gmail.com>
2025-11-14 10:34:18 -0500

    feat: add hello world outputs for introduction lesson

---
+++ starter-files/lesson1-introduction/Introduction.java
@@ -0,0 +1,11 @@
+/******************************************************************************
+
+Introducción a la programación y Java (¿Qué es Java? ¿Por qué usarlo?)
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		//TODO: Imprimir en consola "¡Hola, Mundo!"
+	}
+}
